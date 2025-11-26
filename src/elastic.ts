13a41663bff0f075a8cb199fda558d48e9773e9c
thanhdev1710 <chithanh171004@gmail.com>
2025-11-26 21:43:10 +0700

    update

--- src/elastic.ts
+++ src/elastic.ts
@@ -1,5 +1,5 @@
 import { Client } from "@elastic/elasticsearch";
 
 export const es = new Client({
-  node: process.env.ELASTICSEARCH_URL || "http://elasticsearch:9200",
+  node: process.env.ELASTICSEARCH_URL || "https://elasticsearch:9200",
 });
